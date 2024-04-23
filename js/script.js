f29dd6badd3c35765a19b634220c7b9198eb9948
Dede Supriatna <dedesupriatna1421@gmail.com>
2024-04-24 01:43:52 +0700

    add reset button and formula section

--- js/script.js
+++ js/script.js
@@ -3,14 +3,26 @@ const leftTemperatureUnit = document.querySelector('#left-temperature-unit');
 const rightInput = document.querySelector('#right-input-temperature');
 const rightTemperatureUnit = document.querySelector('#right-temperature-unit');
 const calculationProcessDisplay = document.querySelector('#calculation-process-display');
-const tittleText = document.querySelector('#title-text');
 const formulaDisplay = document.querySelector('#formula-display');
+const formulaExplanationDisplayOne = document.querySelector('#formula-explanation-display-one');
+const formulaExplanationDisplayTwo = document.querySelector('#formula-explanation-display-two');
+const where = document.querySelector('#where');
 const reverseButton = document.querySelector('#reverse-button');
+const resetButton = document.querySelector('#reset-button');
 let result = "";
 let unit = '';
 let inputUnitCharacter = "";
 let outputUnitCharacter = "";
 
+resetButton.addEventListener('click', function(){
+    const input = '';
+    const display = rightInput;
+    const inputUnit = 'celcius';
+    leftInput.value = input;
+    leftTemperatureUnit.value = inputUnit;
+    rightTemperatureUnit.value = inputUnit;
+    calculations(input, display, inputUnit, inputUnit);
+});
 reverseButton.addEventListener('click', function(){
     const input = parseFloat(rightInput.value);
     const display = rightInput;
@@ -52,115 +64,74 @@ rightTemperatureUnit.addEventListener('input', function(){
 
 
 function calculations(input, display, inputUnit, outputUnit){    
+    inputUnitCharacter = units(inputUnit);
+    outputUnitCharacter = units(outputUnit);
+
     if(isNaN(input)){
         input = 0;
     }
-    calculationProcessDisplay.innerHTML = `${input}${unit} = ${input}${unit}`;
 
     if (inputUnit === outputUnit) {
         result = input;
         display.value = result;
         unit = units(inputUnit);
         calculationProcessDisplay.innerHTML = `${input}${unit} = ${input}${unit}`;
-        tittleText.innerHTML = ":";
         formulaDisplay.innerHTML = "";
+        where.innerHTML = '';
+        formulaExplanationDisplayOne.innerHTML = '';
+        formulaExplanationDisplayTwo.innerHTML = '';
+        return;
     } else if(inputUnit === 'celcius' && outputUnit === 'kelvin') {
         result = input+ 273.15;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} + 273.15 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} + 273.15`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> + 273.15`;
     } else if(inputUnit === 'celcius' && outputUnit === 'fahrenheit') {
         result = input * 9 / 5 + 32;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} * 9 / 5 + 32 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} * 9 / 5 + 32`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> * 9 / 5 + 32`;
     } else if(inputUnit === 'celcius' && outputUnit === 'reaumur') {
         result = input * 4 / 5;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} * 4 / 5 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} * 4 / 5`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> * 4 / 5`;
     } else if(inputUnit === 'kelvin' && outputUnit === 'celcius') {
         result = input - 273.15;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} - 273.15 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} - 273.15`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> - 273.15`;
     } else if(inputUnit === 'kelvin' && outputUnit === 'fahrenheit') {
         result = (input - 273.15) * 9 / 5 + 32
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `(${input}${inputUnitCharacter} - 273.15) * 9 / 5 + 32 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = (${inputUnitCharacter} - 273.15) * 9 / 5 + 32`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = (S<sub>(${inputUnitCharacter})</sub> - 273.15) * 9 / 5 + 32`;
     } else if(inputUnit === 'kelvin' && outputUnit === 'reaumur') {
         result = (input - 273.15) * 4 / 5;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `(${input}${inputUnitCharacter} - 273.15) * 4 / 5 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = (${inputUnitCharacter} - 273.15) * 4 / 5`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = (S<sub>(${inputUnitCharacter})</sub> - 273.15) * 4 / 5`;
     } else if(inputUnit === 'fahrenheit' && outputUnit === 'celcius') {
         result = (input - 32) * 5 / 9;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `(${input}${inputUnitCharacter} - 32) * 5 / 9 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = (${inputUnitCharacter} - 32) * 5 / 9`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = (S<sub>(${inputUnitCharacter})</sub> - 32) * 5 / 9`;
     } else if(inputUnit === 'fahrenheit' && outputUnit === 'kelvin') {
         result = (input - 32) * 5 / 9 + 273.15;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `(${input}${inputUnitCharacter} - 32) * 5 / 9 + 273.15 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = (${inputUnitCharacter} - 32) * 5 / 9 + 273.15`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = (S<sub>(${inputUnitCharacter})</sub> - 32) * 5 / 9 + 273.15`;
     } else if(inputUnit === 'fahrenheit' && outputUnit === 'reaumur') {
         result = (input - 32) * 4 / 9;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `(${input}${inputUnitCharacter} - 32) * 4 / 9 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = (${inputUnitCharacter} - 32) * 4 / 9`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = (S<sub>(${inputUnitCharacter})</sub> - 32) * 4 / 9`;
     } else if(inputUnit === 'reaumur' && outputUnit === 'celcius') {
         result = input * 5 / 4;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} * 5 / 4 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} * 5 / 4`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> * 5 / 4`;
     } else if(inputUnit === 'reaumur' && outputUnit === 'kelvin') {
         result = input * 5 / 4 + 273.15;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} * 5 / 4 + 273.15 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} * 5 / 4 + 273.15`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> * 5 / 4 + 273.15`;
     } else if(inputUnit === 'reaumur' && outputUnit === 'fahrenheit') {
         result = input * 9 / 4 + 32;
-        display.value = result;
-        inputUnitCharacter = units(inputUnit);
-        outputUnitCharacter = units(outputUnit);
         calculationProcessDisplay.innerHTML = `${input}${inputUnitCharacter} * 9 / 4 + 32 = ${result}${outputUnitCharacter}`;
-        // tittleText.innerHTML = " dari " + inputUnit + "(" + inputUnitCharacter + ") ke " + outputUnit + "(" + outputUnitCharacter + ") adalah:";
-        formulaDisplay.innerHTML = `${outputUnitCharacter} = ${inputUnitCharacter} * 9 / 4 + 32`;
+        formulaDisplay.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = S<sub>(${inputUnitCharacter})</sub> * 9 / 4 + 32`;
     }
+    display.value = result;
+    formulas(inputUnit, outputUnit, inputUnitCharacter, outputUnitCharacter);
 }
 function units(inputUnit) {
     if(inputUnit === 'celcius'){
@@ -173,4 +144,12 @@ function units(inputUnit) {
         unit = '&deg;R';
     }
     return unit;
+}
+function formulas(inputUnit, outputUnit, inputUnitCharacter, outputUnitCharacter) {
+    where.innerHTML = "Di mana:"
+    formulaExplanationDisplayOne.innerHTML = `S<sub>(${outputUnitCharacter})</sub> = Suhu dalam ${capitalizeFirstLetter(outputUnit)}`;
+    formulaExplanationDisplayTwo.innerHTML = `S<sub>(${inputUnitCharacter})</sub> = Suhu dalam ${capitalizeFirstLetter(inputUnit)}`;
+}
+function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
 }
